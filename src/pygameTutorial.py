8e872854870e78f73c4fcb54c743bd2349fa17ef
FlorianBruch <fbruch93@gmail.com>
2021-12-09 16:55:10 +0100

    pygame tutorial progress

--- src/pygameTutorial.py
+++ src/pygameTutorial.py
@@ -2,5 +2,60 @@ import sys
 import os
 sys.path.append(os.path.join(sys.path[0],'..','lib'))
 import pygame
+pygame.init()
 
-print("hello")
+win = pygame.display.set_mode((500,500))
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+isJumping = False
+jumpCount = 10
+
+run = True
+
+while run:
+    pygame.time.delay(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel:
+        x += vel
+
+    if not isJumping:    
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+
+        if keys[pygame.K_DOWN] and y < 500 - height - vel:
+            y += vel
+
+        if keys[pygame.K_SPACE]:
+            isJumping = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount**2) * 0.5 * neg 
+            jumpCount -= 1    
+
+        else:
+            isJumping = False
+            jumpCount = 10
+    
+    win.fill((0,0,0))  # Fills the screen with black
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.display.update() 
+    
+pygame.quit()
